426f65e6be48b6741c28b5b6e4a764135c2e4aca
Daniel <shao.danielw@gmail.com>
2022-08-17 02:25:35 -0700

    completed the javascript code and polished up the html and css

--- psg.js
+++ psg.js
@@ -1,23 +1,48 @@
 let playerScore = 0;
 let computerScore = 0;
-let choices = [peanut, squirrel, guy];
 
 function disableButtons() {
-    buttons.forEach(elem => {
-        elem.disabled = true
-    })
+    document.getElementById("1").disabled = true;
+    document.getElementById("2").disabled = true;
+    document.getElementById("3").disabled = true;
 }
+
 
 function computeScore(playerClick){
-    let computerClick = choices[Math.floor(Math.random() * choices.length)]
-    if(computerClick == playerClick){
-    }
+    let result ="";
+    let choices = ['peanut', 'squirrel', 'guy'];
+    let computerClick = choices[Math.floor(Math.random() * choices.length)];
+
+    if ((playerClick == 'squirrel' && computerClick == 'peanut') ||
+    (playerClick == 'guy' && computerClick == 'squirrel') ||
+    (playerClick == 'peanut' && computerClick == 'guy')) {
     
-    document.getElementById('result').innerHTML = result
+        playerScore++;
+        result = ('You win! ' + playerClick + ' beats ' + computerClick + ".");
+
+        if (playerScore == 5) {
+            result += '<br><br>You won the game! Reload the page to play again.';
+            disableButtons();
+        }
+    }
+    else if(computerClick == playerClick){
+        result = "Tie! You both chose " + playerClick + ".";
+    }
+    else{
+        computerScore++;
+        result = ('You lose! ' + computerClick + ' beats ' + playerClick + ".");
+
+        if (computerScore == 5) {
+            result += ' You lost the game! Reload the page to play again.';
+            disableButtons();
+        }
+    }
+
+    document.getElementById('result').innerHTML = result;
+    document.getElementById('player').innerHTML = "Player Score: " + playerScore;
+    document.getElementById('computer').innerHTML = "Computer Score: " + computerScore;
 }
 
-
-
-buttons.forEach(button =>{
-    button.addEventListener('click', computeScore(button.value))
-})
+function alerted(){
+    alert("clicked");
+}
